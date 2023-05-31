70f513d724db00cb1a409912c8007f1c0b3cccb8
Asbel Jhonatan Apaza Romero <asbel.elmec@gmail.com>
2023-05-31 17:29:30 -0400

    feat: changes

--- src/app/core/auth/pages/login-page/login-page.component.ts
+++ src/app/core/auth/pages/login-page/login-page.component.ts
@@ -6,6 +6,12 @@ import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth.service';
 import { TRoutePattern } from 'src/app/utils';
 import { AuthES } from '../../helpers';
+import { isDev } from 'src/app/core/config';
+
+const initLogin = {
+  name: isDev ? 'admin@29' : null,
+  password: isDev ? '12345678' : null,
+};
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -16,8 +22,8 @@ export class LoginPageComponent {
   private router = inject(Router);
 
   form: FormGroup = this.fb.group({
-    name: ['admin@29', [Validators.required, Validators.email]],
-    password: ['12345678', [Validators.required, Validators.minLength(6)]],
+    name: [initLogin.name, [Validators.required, Validators.email]],
+    password: [initLogin.password, [Validators.required, Validators.minLength(8)]],
   });
 
   authEs = AuthES;
